5f626547980ea58dde86eae9e6fad3b29a029040
Calebkillom <Calebkillom@gmail.com>
2023-04-11 08:34:30 +0000

    task 0 & 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cLine = 'C is fun';
+const pyLine = 'Python is cool';
+const jLine = 'JavaScript is amazing';
+
+console.log(cLine);
+console.log(pyLine);
+console.log(jLine);
