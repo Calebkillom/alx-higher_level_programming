7db6adaef855c7137edec2f1df96e089a332661b
Calebkillom <Calebkillom@gmail.com>
2023-04-28 23:40:12 +0000

    task 6&7 correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
     url = argv[1]
     response = requests.get(url)
 
-    if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"Error code: {e.response.status_code}")
     else:
         print(response.text)
