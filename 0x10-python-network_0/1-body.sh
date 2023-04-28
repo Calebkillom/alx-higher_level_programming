a72ba0e9cdd9cd746155b83f505fe832fa376fd0
Calebkillom <calebkillom@gmail.com>
2023-04-28 11:00:52 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
-#!bin/bash
+#!/bin/bash
 # script takes URL, sends a GET request , and displays the body
-
-url="$1"
-
-curl -s -o /dev/null -w "%{http_code}\n" "$url" | {
-    read status_code
-    if [ "$status_code" -eq 200 ]; then
-        curl -s "$url" | tr -d '\r'
-    fi
-}
+curl -sL "$1"
