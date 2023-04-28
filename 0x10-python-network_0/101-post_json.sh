ce14c1ca86fedfe9ec9576bb38b6c2cd7e5a2f8c
Calebkillom <calebkillom@gmail.com>
2023-04-28 12:37:16 -0700

    task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s -X POST -H "Content-Type: application/json" -d '{"key":"value"}' "$1" -D - | awk '/Content-Length/ {print $2}'
+# Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+
