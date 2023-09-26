7b96bafd8981e802202e88e994042a5ba484b68a
Calebbkillom <Calebbkillom@gmail.com>
2023-09-26 18:30:24 +0000

    task_4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2]; // API URL of Star Wars films
+const characterId = 18; // Wedge Antilles character ID
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    try {
+      const filmsData = JSON.parse(body).results;
+      const moviesWithWedgeAntilles = filmsData.filter((film) =>
+        film.characters
+				.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+      );
+
+      console.log(`${moviesWithWedgeAntilles.length}`);
+    } catch (parseError) {
+      console.error(parseError);
+    }
+  }
+});
+
