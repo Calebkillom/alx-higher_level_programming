3a731f57658b026debb8c1ca7a5a605726f1cd24
Calebbkillom <Calebbkillom@gmail.com>
2023-09-26 19:19:24 +0000

    task-6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('API returned a non-200 status code:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      const userId = todo.userId.toString();
+      completedTasksByUser[userId] = (completedTasksByUser[userId] || 0) + 1;
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
