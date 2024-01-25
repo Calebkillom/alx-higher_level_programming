6e5bb7ca6dd76cde2d57b7cb259d656b66e484cb
Calebkillom <Calebkillom@gmail.com>
2024-01-25 11:24:24 +0300

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" function that computes the square value of all integers of a matrix """
+
+def square_matrix_simple(matrix=[]):
+    """ computes the square value of all integers """
+    sqmat = [[element**2 for element in row] for row in matrix]
+    return sqmat
