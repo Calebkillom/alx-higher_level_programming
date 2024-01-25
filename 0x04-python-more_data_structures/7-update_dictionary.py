a833538e6f610a124e001547e17273f5891536d1
Calebkillom <Calebkillom@gmail.com>
2024-01-25 21:31:14 +0300

    task-7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that deletes a key in a dictionary """
+
+
+def update_dictionary(a_dictionary, key, value):
+    """ function that deletes a key in a dictionary """
+    a_dictionary[key] = value
+    return a_dictionary
