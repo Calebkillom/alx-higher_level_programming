822b61eeb04e08ad661bf24dbb8cbd25ff1ddd05
Calebkillom <Calebkillom@gmail.com>
2024-01-27 00:06:43 +0300

    task 12 corr

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
     for i in range(len(roman_string) - 1):
