91ba580c2903d7de812c90db43a3360201029b12
calebkillom <calebkillom@gmail.com>
2024-02-22 05:16:05 +0300

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class definition"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Public instance method that raises an Exception"""
+        raise Exception("area() is not implemented")
