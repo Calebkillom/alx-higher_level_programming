30b181680a631a0b3b592f3eeccbdbe86ba0e252
calebkillom <calebkillom@gmail.com>
2024-02-22 04:01:01 +0300

    task 0 correction

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,14 +4,4 @@
 
 def lookup(obj):
     """Returns list of available attributes and methods of an object."""
-    # empty list
-    available_att = []
-
-    # check the attributes and store in a variable
-    list_of_att = dir(obj)
-
-    # add the variable in the empty list
-    available_att.extend(list_of_att)
-
-    # return the list
-    return available_att
+    return dir(obj)
